4094339601b0b9f54735e52a25cf9203e499dd4f
Fabrizzio De Bracamonte <fabrizziodbc@gmail.com>
2022-02-27 17:45:33 -0500

    fix route

--- App.js
+++ App.js
@@ -33,7 +33,7 @@ export default function App() {
       screens: {
         RentTabNavigation: {
           screens: {
-            MessageScreen: 'messages',
+            OwnerMessagesScreen: 'messages',
           },
         },
       },
